a1e64e317ba5192aacb844f72b905c4b873950a7
benni90100 <micelibenedetto800@gmail.com>
2023-11-20 18:59:36 +0100

    esercizio 1

--- a.js
+++ a.js
@@ -1,1 +1,13 @@
-console.log("ciao");
+const studenti = [
+    { nome: "Alice", eta: 17, mediaVoti: 8.5 },
+    { nome: "Bob", eta: 22, mediaVoti: 7.8 },
+    { nome: "Charlie", eta: 16, mediaVoti: 9.2 },
+    { nome: "David", eta: 23, mediaVoti: 6.5 }
+];
+
+function eMaggiorenne(array) {
+    return array.filter(studente => studente.eta %2 ===0)
+}
+
+let magg = eMaggiorenne(studenti)
+console.log(magg);
