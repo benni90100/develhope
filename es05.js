819b200a966df458620c5197faff982831c16d15
benni90100 <micelibenedetto800@gmail.com>
2023-11-20 19:35:35 +0100

    esercizio5

--- es05.js
+++ es05.js
@@ -1,14 +1,22 @@
-const person1 = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25,
-};
+// Implementare la funzione ageAverage che, dato un array di persone, calcola l'età media.
+function calculateAverageAge(persons) {
+    const sum = persons.reduce((a, person)=> a + person.age, 0)
+    return sum/persons.length
+}
 
-const person2 = person1;
-person2.firstName = "simon"
+const persons = [
+    { name: 'Paul', age: 16 },
+    { name: 'George', age: 17 },
+    { name: 'Lucas', age: 21 },
+    { name: 'Marco', age: 32 },
+    { name: 'Peter', age: 18 },
+    { name: 'Carl', age: 13 },
+    { name: 'Simon', age: 24 },
+    { name: 'Mark', age: 15 },
+    { name: 'Sandra', age: 34 },
+    { name: 'Alice', age: 28 }
+];
 
-// Modify the property `firstName` of the `person2` in "Simon".
-//modificando il valore di person2 automaticamente viene richiamato quello di person 1 perchè abbiamo dichiarato che 
-//person2 = person1 quindi le due const puntano allo stesso oggetto
-console.log(person1);
-console.log(person2);
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
