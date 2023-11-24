1db8a09e1646a1b38f524b9fbeba1bc7bab5410c
benni90100 <micelibenedetto800@gmail.com>
2023-11-24 09:15:52 +0100

    es22.js

--- es22.js
+++ es22.js
@@ -5,8 +5,8 @@
 // Prima dello scadere dei 2 secondi stampa in console il messaggio ""Lancio dei dadi in corso..."
 
 function lancioDadi() {
-    const dado1 = (Math.floor(Math.random()*6))
-    const dado2 = (Math.floor(Math.random()*6))
+    const dado1 = (Math.floor(Math.random()*6)+1)
+    const dado2 = (Math.floor(Math.random()*6)+1)
     console.log(`dado1= ${dado1}, dado2= ${dado2}`);
 }
 setTimeout(lancioDadi, 2000)
