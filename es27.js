d6df22d27737ed9910a0e2c1007472756f2aa163
benni90100 <micelibenedetto800@gmail.com>
2023-11-26 11:11:44 +0100

    es27

---
+++ es27.js
@@ -0,0 +1,22 @@
+// Dato l'oggetto { name: "John", age: 30 } crea una funzione che simula un'operazione asincrona,
+//  come il recupero dei dati da un'API. -Implementare una callback per gestire i dati recuperati.
+//   -Utilizzare setTimeout per simulare il ritardo dell'operazione.
+
+const obj = {
+    name: "John",
+    age: 30
+}
+
+
+function fetchDataFromAPI(callback) {
+    setTimeout(() => {
+        callback(obj)
+    }, 2000);
+}
+
+function handleData(data) {
+    return console.log(`${data.name } ${data.age}`);
+    
+}
+
+fetchDataFromAPI(handleData);
