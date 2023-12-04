16163a35549d3a734b9ad6c6f5db2ef48d14b9a3
benni90100 <micelibenedetto800@gmail.com>
2023-12-04 07:40:58 +0100

    es31

--- es31.js
+++ es31.js
@@ -1,10 +1,20 @@
-function log() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("ciao a tutti")
-        }, 2000);
-    })
-}
-log()
-.then(data=> console.log(data))
-.catch(err=> console.error(err))
+// function log() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("ciao a tutti")
+//         }, 2000);
+//     })
+// }
+// log()
+// .then(data=> console.log(data))
+// .catch(err=> console.error(err))
+
+
+// potevamo anche non scrivere la funzione, in questo caso:
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("ciao a tutti")
+    }, 2000);
+})
+.then(data=>console.log(data))
+.catch(err=>console.error(err))
