57ace9f8e050fbd04170c4fea4131c178ed5c94c
benni90100 <micelibenedetto800@gmail.com>
2023-12-11 19:22:32 +0100

    todo

---
+++ todolist/app.js
@@ -0,0 +1,15 @@
+const insert = document.querySelector("#insert")
+const input = document.querySelector("#TODO")
+let tasks = []
+
+function newTask(task) {
+    tasks.push(task)
+}
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    input.addEventListener("click",()=>
+    input.
+
+    )
+})
