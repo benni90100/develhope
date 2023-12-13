f7fd9df6cb33e30643a69e7859d8028a2eed35e2
benni90100 <micelibenedetto800@gmail.com>
2023-12-13 21:22:02 +0100

    add

--- dinamic-card/app.js
+++ dinamic-card/app.js
@@ -1,19 +1,23 @@
 const cardContainer = document.querySelector(".card-container")
-
+const input = document.querySelector("#input")
+const button = document.querySelector("button")
+const img = document.createElement("img")
+const title = document.createElement("h2")
 async function getData() {
     try {
-        const response = await fetch("https://ringsdb.com/api/public/card/01001.json")
+        const response = await fetch(`https://ringsdb.com/api/public/card/${input.value}.json`)
         const data = await response.json()
-
-        const img = document.createElement("img")
         img.setAttribute("src", `https://ringsdb.com/${data.imagesrc}`)
-        const title = document.createElement("h2")
+        img.classList.add("rotate-center", "card")
         title.innerText = data.name
+        
         cardContainer.append( title, img)
         console.log(data);
+        
     } catch (error) {
         console.error(error);
     }
 
 }
-getData()
+
+button.addEventListener("click", getData)
