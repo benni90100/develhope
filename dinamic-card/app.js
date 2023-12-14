cf5c45b739a957e99e2fd3f4e14627c1dbf6e861
benni90100 <micelibenedetto800@gmail.com>
2023-12-14 09:52:12 +0100

    dev

--- dinamic-card/app.js
+++ dinamic-card/app.js
@@ -3,6 +3,7 @@ const input = document.querySelector("#input")
 const button = document.querySelector("button")
 const img = document.createElement("img")
 const title = document.createElement("h2")
+
 async function getData() {
     try {
         const response = await fetch(`https://ringsdb.com/api/public/card/${input.value}.json`)
