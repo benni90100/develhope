17ccc190714733b334d5f131a9ce200c7d8dd39a
benni90100 <micelibenedetto800@gmail.com>
2023-12-14 12:27:08 +0100

    dev

--- dinamic-card/app.js
+++ dinamic-card/app.js
@@ -6,19 +6,21 @@ const title = document.createElement("h2")
 
 async function getData() {
     try {
+        document.getElementById('loading-icon').style.display = 'block';
         const response = await fetch(`https://ringsdb.com/api/public/card/${input.value}.json`)
         const data = await response.json()
         img.setAttribute("src", `https://ringsdb.com/${data.imagesrc}`)
         img.classList.add("rotate-center", "card")
         title.innerText = data.name
-        
         cardContainer.append( title, img)
         console.log(data);
+        document.getElementById('loading-icon').style.display = 'none';
         
     } catch (error) {
         console.error(error);
     }
 
 }
+console.log(getData);
 
 button.addEventListener("click", getData)
